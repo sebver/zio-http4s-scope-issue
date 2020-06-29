ea3d3deda9173a74ea52f676b19d91f0d71b0852
Sebastian Vercruysse <sebastian.vercruysse@gmail.com>
2020-06-29 15:46:50 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "zio-scope-issue"
+
+version := "0.1"
+
+scalaVersion := "2.13.2"
+
+val zioVersion            = "1.0.0-RC21-1"
+val zioInteropCatsVersion = "2.1.3.0-RC16"
+val http4sVersion         = "0.21.6"
+
+fork in run := true
+
+libraryDependencies ++= Seq(
+  "dev.zio"      %% "zio"                       % zioVersion,
+  "dev.zio"      %% "zio-interop-cats"          % zioInteropCatsVersion,
+  "org.http4s"   %% "http4s-core"               % http4sVersion,
+  "org.http4s"   %% "http4s-dsl"                % http4sVersion,
+  "org.http4s"   %% "http4s-blaze-server"       % http4sVersion,
+)
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,44 @@
+import org.http4s.HttpRoutes
+import org.http4s.dsl.Http4sDsl
+import org.http4s.implicits._
+import org.http4s.server.blaze.BlazeServerBuilder
+import zio.{Task, ZIO, ZManaged}
+import zio._
+import zio.clock._
+import zio.internal.Platform
+import zio.interop.catz._
+import zio.interop.catz.implicits._
+
+import scala.concurrent.ExecutionContext
+
+object Main extends App with Http4sDsl[Task] {
+
+  override val platform =
+    Platform.default.withReportFailure(cause =>
+      if (!cause.interrupted) println(cause.prettyPrint))
+
+  val serverManaged: ZManaged[Any, Throwable, Unit] = server(
+    HttpRoutes.of[Task] {
+      case GET -> Root / "status" =>
+        Ok()
+    })
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    serverManaged.useForever.exitCode
+
+  private def server(routes: HttpRoutes[Task]): ZManaged[Any, Throwable, Unit] =
+    withConcurrentEffect { implicit runtime =>
+      BlazeServerBuilder[Task](ExecutionContext.global)
+        .bindHttp(8080, "0.0.0.0")
+        .withHttpApp(routes.orNotFound)
+        .withoutBanner
+        .resource
+        .toManaged
+        .unit
+    } provideLayer Clock.live
+
+  private def withConcurrentEffect[R <: Clock, E, A](
+      f: Runtime[Clock] => ZManaged[R, E, A]): ZManaged[R, E, A] =
+    ZIO.runtime[Clock].toManaged_.flatMap(f)
+
+}
